09a8029bd0a85350fd6857b025bd8e64dbf196fa
Ivan Lougan <robinio123@gmail.com>
2023-02-01 16:03:27 +0000

    add changes into css and js code

--- dino.js
+++ dino.js
@@ -1,15 +1,52 @@
 class Dinosaur {
     constructor (name) {
         this.name = name;
-        this.health = 100;
+        this.rest = 100;
         this.hunger = 100;
-        this.thirsty = 100;
+        this.thirst = 100;
+        this.happiness = 100;
     }
-    eat() {
-        // -10 thirsty?? or/and +5 health
+
+
+    //    runFromMeteorite -10health, -10 hunger, -10 thirst.
+    runFromMeteorite() {
+        this.rest -= 10;
+        this.hunger -= 10;
+        this.thirst -= 10;
+        this.happiness -= 15;
     }
+
+    //    Sleep +10health +20 rest
+    sleep() {
+        this.hunger -= 20;
+        this.thirst -= 20;
+        this.rest += 20;
+        this.happiness += 20;
+    }
+
+    //    fightForFood -20 health, -10hunger, -20 rest
+    fightForFood() {
+        this.rest -= 20;
+        this.hunger += 10;
+        this.thirst -= 10;
+    }
+
+
+
+    //    trainChildren: +10 children, +5 health, -5 hunger, -5 thirst, -10 rest.
+    trainChildren() {
+        this.rest -= 10;
+        this.hunger -= 5;
+        this.thirst -= 5;
+        this.happiness += 15;
+    }
+
+
+    //    Drink: +10 thirst, +5health
     drink() {
-        // +10 thirsty?
+        this.thirst += 10;
+        this.rest += 5;
+        this.happiness += 10;
     }
 }
 
