b3a74c48fbc2220940006512f60b179dff06d024
Ivan Lougan <robinio123@gmail.com>
2023-02-01 17:14:33 +0000

    add layout plus some logic

--- dino.js
+++ dino.js
@@ -7,8 +7,6 @@ class Dinosaur {
         this.happiness = 100;
     }
 
-
-    //    runFromMeteorite -10health, -10 hunger, -10 thirst.
     runFromMeteorite() {
         this.rest -= 10;
         this.hunger -= 10;
@@ -16,7 +14,6 @@ class Dinosaur {
         this.happiness -= 15;
     }
 
-    //    Sleep +10health +20 rest
     sleep() {
         this.hunger -= 20;
         this.thirst -= 20;
@@ -24,16 +21,12 @@ class Dinosaur {
         this.happiness += 20;
     }
 
-    //    fightForFood -20 health, -10hunger, -20 rest
     fightForFood() {
         this.rest -= 20;
         this.hunger += 10;
         this.thirst -= 10;
     }
 
-
-
-    //    trainChildren: +10 children, +5 health, -5 hunger, -5 thirst, -10 rest.
     trainChildren() {
         this.rest -= 10;
         this.hunger -= 5;
@@ -41,8 +34,6 @@ class Dinosaur {
         this.happiness += 15;
     }
 
-
-    //    Drink: +10 thirst, +5health
     drink() {
         this.thirst += 10;
         this.rest += 5;
@@ -60,5 +51,20 @@ class Trex extends Dinosaur {
     }
 }
 
+
+
+
+
+
+
 let trex = new Trex("Puppy");
 console.log(trex);
+
+
+const createDinoButton = document.getElementById("createButton");
+const menuStart = document.querySelector("#menuStart");
+
+createDinoButton.addEventListener("click", () => {    
+        menuStart.style.display = "none";
+    
+});
